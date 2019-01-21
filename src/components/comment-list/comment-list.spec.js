a167c7a7a93f8414bfb037da451bc24079b437f0
Ana_Ska <anna.skalabava@dizzain.com>
2019-01-21 14:24:32 +0300

    test to comment list added

---
+++ src/components/comment-list/comment-list.spec.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Enzyme, {mount} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import CommentList from './index';
+import mockedComments from '../../fixtures';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('Comment List', function () {
+    it('should render only button and no comments by default', () => {
+        const wrapper = mount(
+            <CommentList comments={mockedComments[0].comments} isOpen />
+        );
+
+        expect(wrapper.find('.test--comment__btn').length)
+            .toEqual(1);
+
+        expect(wrapper.find('.test--comment__container').length)
+            .toEqual(0);
+    });
+
+    it('should show comment list after click on button', () => {
+        const wrapper = mount(
+            <CommentList comments={mockedComments[0].comments} isOpen />
+        );
+
+        wrapper.find('.test--comment__btn').at(0).simulate('click');
+
+        expect(wrapper.find('.test--comment__container').length)
+            .toEqual(5);
+    });
+});
