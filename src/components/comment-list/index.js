d2a017a5ad41a092e8ed5c7141885d4da302f8af
Ana_Ska <anna.skalabava@dizzain.com>
2019-01-21 13:29:50 +0300

    react css animation added to comment list

--- src/components/comment-list/index.js
+++ src/components/comment-list/index.js
@@ -1,7 +1,9 @@
-import React, { Component } from 'react'
-import Comment from './comment'
-import toggleOpen from '../decorators/toggleOpen'
+import React, {Component} from 'react'
+import Comment from '../comment'
+import toggleOpen from '../../decorators/toggleOpen'
 import PropTypes from 'prop-types';
+import CSSTransition from 'react-addons-css-transition-group';
+import './comment-list.css';
 
 class CommentList extends Component {
     static propTypes = {
@@ -10,34 +12,42 @@ class CommentList extends Component {
         // from decorator
         isOpen: PropTypes.bool,
         toggleOpenItem: PropTypes.func.isRequired
-    }
+    };
 
     render() {
-        const { isOpen, toggleOpenItem } = this.props
+        const {isOpen, toggleOpenItem} = this.props;
         return (
             <div>
                 <button onClick={toggleOpenItem}>
                     {isOpen ? 'hide comments' : 'show comments'}
                 </button>
-                {isOpen ? this.getBody() : null}
+                <CSSTransition
+                    transitionName="comment-list"
+                    transitionEnterTimeout={200}
+                    transitionLeaveTimeout={200}
+                >
+                    {this.getBody()}
+                </CSSTransition>
             </div>
         )
     }
 
     getBody() {
-        const { comments } = this.props
+        const {comments, isOpen} = this.props;
         const body = comments.length ? (
             <ul>
                 {comments.map((comment) => (
                     <li key={comment.id}>
-                        <Comment comment={comment} />
+                        <Comment comment={comment}/>
                     </li>
                 ))}
             </ul>
         ) : (
             <h3>No comments yet</h3>
-        )
-        return <div>{body}</div>
+        );
+        return isOpen ? (
+            <div>{body}</div>
+        ) : null
     }
 }
 
